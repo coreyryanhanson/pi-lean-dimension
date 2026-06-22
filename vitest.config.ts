6516d06f4cbcee9fcf690a8ab050e44b2d071b96
Corey Ryan Hanson <17171838+coreyryanhanson@users.noreply.github.com>
2026-06-22 15:13:49 +0000

    initial packaging work

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
-		globals: true, // allow describe/it without imports
-		include: ["__tests__/**/*.test.ts", "core/archived/**/*.test.ts"],
+		globals: true,
+		include: ["packages/*/**/*.test.ts"],
+		passWithNoTests: true,
 	},
 });
